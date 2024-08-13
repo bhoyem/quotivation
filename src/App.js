fa26f3ca3f00704862231bf30ef09bac52b00d36
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-08-13 06:26:39 -0500

    Added coding to select quotes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Loader } from "react-feather";
 import Quotes from "./components/quotes/Quotes.js";
+import CategoryForm from "./components/quotes/CategoryForm.js";
 import "./App.css";
 
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [category, setCategory] = useState(["All"]);
+
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+  const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
 
   const fetchQuotes = async () => {
     try {
@@ -31,7 +35,7 @@ function App() {
     <div className='App'>
       <Header />
       <main>
-        {loading ? <Loader /> : <Quotes quotes={quotes} /> /*JSON.stringify(quotes)*/} 
+        {loading ? (<Loader />) : (<Quotes quotes={quotes} categories={categories} category={category} />) /*JSON.stringify(quotes)*/}
       </main>
       <Footer />
     </div>
--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,10 +1,15 @@
 import React from "react";
-import QuoteCard from "./QuoteCard";
+import QuoteCard from "./QuoteCard.js";
+import CategoryForm from "./CategoryForm.js";
 
-function Quotes({ quotes }) {
+function Quotes({ quotes, categories, category }) {
     return (
         <section className="all-quotes">
             <div className="quotes wrapper">
+                <div className="categoryHeader">
+                    <p>Browse through your collection of quotes</p>
+                    <CategoryForm categories={categories} category={category} />
+                </div>
                 {quotes.map((quote) => (<QuoteCard key={quote.id} quote={quote} />))}
             </div>
         </section>
