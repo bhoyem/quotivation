d4bc7c913ab3963313948a23aa061d09e0ecb838
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-08-15 14:53:10 -0500

    Added coding to display messages on screen when editing list of favorite quotes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import { Loader } from "react-feather";
 import Quotes from "./components/quotes/Quotes.js";
 import FavoriteQuotes from "./components/quotes/FavoriteQuotes.js";
+import Message from "./components/Message.js";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState("All");
   const [favoriteQuotes, setFavoriteQuotes] = useState([]);
+  const [messageText, setMessageText] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
 
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
@@ -43,15 +46,24 @@ function App() {
 
     // console.log(selectedQuote);
     if (alreadyFavorite) {
-      console.log("This quote is already in your favorites! Choose another");
+      removeFromFavorites(quoteId);
+      setMessageText("Removed from favorites");
+      setShowMessage(true);
     } else if (favoriteQuotes.length < maxFaves) {
       setFavoriteQuotes([selectedQuote, ...favoriteQuotes]);
-      console.log("Added to favorites!")
+      setMessageText("Added to favorites!");
+      setShowMessage(true);
     } else {
-      console.log("Max number of Favorite Quotes reached. Please delete one to add another.")
+      setMessageText("Max number of Favorite Quotes reached. Please delete one to add another.");
+      setShowMessage(true);
     };
     return;
   }
+
+  const removeMessage = () => {
+    setShowMessage(false);
+  };
+
 
   const removeFromFavorites = (quoteId) => {
     // console.log(quoteId)
@@ -65,10 +77,11 @@ function App() {
   return (
 
     <div className='App'>
+      {showMessage && <Message messageText={messageText} removeMessage={removeMessage} />}
       <Header />
       <main>
         <FavoriteQuotes favoriteQuotes={favoriteQuotes} maxFaves={maxFaves} removeFromFavorites={removeFromFavorites} />
-        {loading ? (<Loader />) : (<Quotes quotes={filteredQuotes} categories={categories} category={category} handleCategoryChange={handleCategoryChange} addToFavorites={addToFavorites} favoriteQuotes = {favoriteQuotes}/>)}
+        {loading ? (<Loader />) : (<Quotes quotes={filteredQuotes} categories={categories} category={category} handleCategoryChange={handleCategoryChange} addToFavorites={addToFavorites} favoriteQuotes={favoriteQuotes} />)}
       </main>
       <Footer />
     </div>
---
+++ src/components/Message.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Message({ messageText, removeMessage }) {
+
+return (
+    <div className="message">
+        <p>
+            {messageText}
+        </p>
+        <span className="close-message" onClick={removeMessage}>x</span>
+
+    </div>
+)};
+
+export default Message;
