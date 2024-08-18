cdd5768727185bc114763abfa3574a8eaedb3337
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-08-18 06:23:38 -0500

    Corrected a warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, Component } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Loader } from "react-feather";
